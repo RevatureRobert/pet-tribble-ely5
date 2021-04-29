a3876472ee5e0d4ecf4ed184a6f8932216b9e15b
ely5 <ely5@case.edu>
2021-04-29 18:01:08 -0400

    models

--- src/main/java/models/MyTribble.java
+++ src/main/java/models/MyTribble.java
@@ -12,6 +12,10 @@ import javax.persistence.*;
 @NoArgsConstructor
 public class MyTribble {
 
+    @ManyToOne(fetch=FetchType.LAZY)
+    @JoinColumn(name="OWNER_ID")
+    private MyLab myLab;
+
     @Id
     @GeneratedValue
     int id;

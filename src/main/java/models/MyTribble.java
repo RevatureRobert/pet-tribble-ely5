02f9b3cc6ba0591028d4446ca31e63fdb328c1f6
ely5 <ely5@case.edu>
2021-04-29 17:40:00 -0400

    setup

---
+++ src/main/java/models/MyTribble.java
@@ -0,0 +1,16 @@
+package models;
+
+import lombok.*;
+
+@Getter
+@Setter
+@ToString
+@EqualsAndHashCode
+@AllArgsConstructor
+@NoArgsConstructor
+class MyTribble {
+
+    int id;
+    String name;
+    int age;
+}
